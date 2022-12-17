e114dc9e75b8b952ccbaa10afb889efddcc5a644
Rachit Garg <rachitgarg900@gmail.com>
2022-12-17 23:32:23 +0100

    add implementation steps and skeleton to main.cpp

--- task_9_year_calendar/main.cpp
+++ task_9_year_calendar/main.cpp
@@ -1,1 +1,18 @@
+// TODO
+/*
+no of days array for each month
+insert month name, days columns
+insert spaces equal to empty days
+loop for each element no of days time
+insert new line after day 7 otherwise space
+continue printing 
+*/
 
+#include <iotstream>
+#include <iomanip>
+
+int main()
+{
+  // Main Logic goes here
+  return 0;
+}

4a0c0ef46bd167bb9074d029870adc55f346f985
Rachit Garg <rachitgarg900@gmail.com>
2023-01-11 12:58:15 +0100

    Added task 12 code

--- task_12_concepts_concatenate_different_nums/main.cpp
+++ task_12_concepts_concatenate_different_nums/main.cpp
@@ -1,1 +1,23 @@
-TODO
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+concept ConvertibleToStdString = requires( T n) {
+                                     std::to_string(n);
+                                 };
+
+ 
+template <typename T, typename P>
+requires ConvertibleToStdString<T> && ConvertibleToStdString<P>
+ auto concatenate(const T   n1, const P  n2) 
+ ->decltype(std::to_string(n1) + std::to_string(n2)) 
+{
+    return std::to_string(n1) + std::to_string(n2);
+}
+
+int main()
+{
+    auto result = concatenate(11,22.22);
+    std::cout << "result : " << result << std::endl;
+    return 0;
+}

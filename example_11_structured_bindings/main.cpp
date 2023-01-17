ecaeb9350e1d66e729188f9e847febfe80964a94
Rachit Garg <rachitgarg900@gmail.com>
2023-01-17 23:20:01 +0100

    Added example 11

--- example_11_structured_bindings/main.cpp
+++ example_11_structured_bindings/main.cpp
@@ -1,1 +1,33 @@
-TODO
+#include <iostream>
+
+struct Point 
+{
+    double x{}, y{};
+};
+
+int main()
+{
+    Point p;
+
+    p.x = 10;
+    p.y = 11;
+
+    // With the help of structured bindings, copies of public members can be extracted 
+    // in the order of their declaration
+    auto [x, y] = p;
+
+    std::cout << "x : " << x << std::endl;
+    std::cout << "y : " << y << std::endl;
+
+    // Capturing structured bindings only possible from C++20 onwards
+    // works with & and = as well 
+    [x, y] ()
+    {
+        std::cout << "\nInside Lambda\n";
+        std::cout << "x : " << x << std::endl;
+        std::cout << "y : " << y << std::endl;
+    }();
+
+    return 0;
+
+}

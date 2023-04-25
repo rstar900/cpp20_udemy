1d905aad66b54a312429fcb5fabed9a7564576b7
Rachit Garg <rachitgarg900@gmail.com>
2023-04-25 17:50:34 +0200

    Added task 18 code

--- task_18_read_files_like_a_gentleman/main.cpp
+++ task_18_read_files_like_a_gentleman/main.cpp
@@ -1,1 +1,52 @@
-TODO
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+
+void read_from_file(std::string_view filename, size_t size, int * data)
+{
+    std::ifstream fstrem_in{ filename.data() }; // We may fail to open the file
+
+    
+    if (fstrem_in.fail()) {
+        //We failed to open the file
+        std::string err_msg = std::string("Unable to open file ") + std::string(filename.data());
+        throw std::runtime_error(err_msg);
+    }
+    
+    int value;
+    int index{};
+    while (fstrem_in >> value) {
+        data[index++] = value;
+    }
+    //Something might go wrong in the process of reading the file.
+
+    if (!fstrem_in.eof()) { 
+        //We failed to reach End of File
+        std::string err_msg = std::string("Something went wrong while reading the file ") + std::string(filename.data());
+        throw std::runtime_error(err_msg);
+    }
+ 
+}
+
+int main()
+{
+    int data[4]{};
+ 
+    try{
+
+    read_from_file("numbersss.txt",std::size(data),data);
+
+    for(auto i : data){
+        std::cout << "value : " << i << std::endl;
+    }
+
+    } catch(std::runtime_error& e) {
+
+        std::cout << e.what() << std::endl;
+        return 1;
+        
+    }
+
+    return 0;
+}

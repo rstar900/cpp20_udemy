35bd2ba87171e13f06756c937325c8016f254387
Rachit Garg <rachitgarg900@gmail.com>
2023-04-26 13:39:00 +0200

    Added task 16 code

--- task_16_default_ordered_points/main.cpp
+++ task_16_default_ordered_points/main.cpp
@@ -1,1 +1,38 @@
-TODO
+#include "point.h"
+#include <iostream>
+
+int main()
+{
+    Point p1(1,1);
+    Point p2(2,2);
+ 
+    std::cout << std::endl;
+    std::cout << "p1 > p2 : " << (p1 > p2) << std::endl;
+    std::cout << "p1 > p2 : " << (p1 > 2) << std::endl; // Implicit conversion from left
+    std::cout << "p1 > p2 : " << (1 > p2) << std::endl; // Implicit conversion from right
+ 
+    std::cout << std::endl;
+    std::cout << "p1 < p2 : " << (p1 < p2) << std::endl;
+    std::cout << "p1 < p2 : " << (p1 < 2) << std::endl; // Implicit conversion from left
+    std::cout << "p1 < p2 : " << (1 < p2) << std::endl; // Implicit conversion from right
+ 
+    std::cout << std::endl;
+    std::cout << "p1 >= p2 : " << (p1 >= p2) << std::endl;
+    std::cout << "p1 >= p2 : " << (p1 >= 2) << std::endl; // Implicit conversion from left
+    std::cout << "p1 >= p2 : " << (1 >= p2) << std::endl; // Implicit conversion from right
+ 
+    std::cout << std::endl;
+    std::cout << "p1 <= p2 : " << (p1 <= p2) << std::endl;
+    std::cout << "p1 <= p2 : " << (p1 <= 2) << std::endl; // Implicit conversion from left
+    std::cout << "p1 <= p2 : " << (1 <= p2) << std::endl; // Implicit conversion from right
+ 
+    std::cout << std::endl;
+    std::cout << "p1 == p2 : " << (p1 == p2) << std::endl;
+    std::cout << "p1 == p2 : " << (p1 == 2) << std::endl; // Implicit conversion from left
+    std::cout << "p1 == p2 : " << (1 == p2) << std::endl; // Implicit conversion from right
+ 
+    std::cout << std::endl;
+    std::cout << "p1 != p2 : " << (p1 != p2) << std::endl;
+    std::cout << "p1 != p2 : " << (p1 != 2) << std::endl; // Implicit conversion from left
+    std::cout << "p1 != p2 : " << (1 != p2) << std::endl; // Implicit conversion from right
+}
---
+++ task_16_default_ordered_points/point.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <compare>
+
+struct Point {
+    Point (double x, double y) : m_x(x), m_y(y) {}
+    Point(double x) : Point(x,x) {}
+    ~Point(){
+    }
+ 
+    double m_x{};
+    double m_y{};
+
+    // Compiler generates all comparison operator for default
+    auto operator<=>(const Point& right) const = default;
+};
+
+// defining outside to enable implicit conversions on left operand 
+auto operator<=>(const double left, const Point& right) {
+    return Point(left).operator<=>(right);
+}

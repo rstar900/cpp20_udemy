fc87c9fcf0613f590bb00598d8fab9d5e18b36b3
Rachit Garg <rachitgarg900@gmail.com>
2023-04-26 13:18:38 +0200

    Added task 15 code

--- task_15_are_cu_strings_equal_cpp20/main.cpp
+++ task_15_are_cu_strings_equal_cpp20/main.cpp
@@ -1,1 +1,14 @@
-TODO
+#include "my_string.h"
+
+int main()
+{
+    CU::string a("Hello");
+    CU::string b("Hello");
+ 
+    auto result1 = (a == b);
+    auto result2 = (a!=b);
+    std::cout << std::boolalpha;
+    std::cout << "result1 : " << result1 << std::endl;
+    std::cout << "result2 : " << result2 << std::endl;
+    return 0;
+}

9d7717ef56ea3b6fa16ca28c6f8db0618dc06c9d
Rachit Garg <rachitgarg900@gmail.com>
2023-04-26 12:39:22 +0200

    Added task 13 complete code

--- task_13_own_string_class/main.cpp
+++ task_13_own_string_class/main.cpp
@@ -1,1 +1,51 @@
-TODO
+#include "my_string.h"
+
+int main() {
+    CU::string str1 {"to be question"};
+    CU::string str2 {"the "};
+
+    std::cout << "---------------------------------------------------------\n";
+
+    str1.insert(6, str2);
+    std::cout << "After doing str1.insert(6, str2) : " << str1 << std::endl;
+    std::cout << "Size: " << str1.size() << std::endl;
+    std::cout << "Capacity: " << str1.capacity() << std::endl;
+
+    std::cout << "---------------------------------------------------------\n";
+
+    str1.clear();
+    std::cout << "After doing str1.clear() : " << str1 << std::endl;
+    std::cout << "Size: " << str1.size() << std::endl;
+    std::cout << "Capacity: " << str1.capacity() << std::endl;
+
+    std::cout << "---------------------------------------------------------\n";
+
+    CU::string str3 {"This is an example sentence."};
+    str3.erase(10,8);
+    std::cout << "After doing str3.erase(0, 8) : " << str3 << std::endl;
+    std::cout << "Size: " << str3.size() << std::endl;
+    std::cout << "Capacity: " << str3.capacity() << std::endl;
+
+    std::cout << "---------------------------------------------------------\n";
+
+    CU::string str4 ("green apple");
+    CU::string str5 ("red apple");
+
+    if (str4.compare(str5) != 0)
+        std::cout << str4 << " is not " << str5 << '\n';
+
+    CU::string str6 ("apple");
+    CU::string str7 ("apple");
+
+    if (str6.compare(str7) == 0)
+        std::cout << "Still, an " << str6 << " is an " << str7 << '\n';
+
+    std::cout << "---------------------------------------------------------\n";
+
+    CU::string str8 = "Writing ";
+    CU::string str9 = "print 10 and then 5 more";
+    str8.append(str9);
+    std::cout << "After str8.append(str9) : " << str8 << '\n';
+
+    std::cout << "---------------------------------------------------------\n";
+}
--- task_13_own_string_class/my_string.h
+++ task_13_own_string_class/my_string.h
@@ -11,6 +11,12 @@ class string{
 	public : 
 	 string(const char* data, size_t size = 15 ){
 		m_size = size;
+
+		// This needs to be there to support cstrings greater than length of 15
+		if (int s = std::strlen(data); s > 15) {
+			m_size = s;
+		}
+
 		m_data = new char [m_size];
 		std::strcpy(m_data,data );
 	}
@@ -37,6 +43,86 @@ class string{
 		return m_data;
 	}
 
+   // return the size of string
+	int size() const {
+		return std::strlen(m_data);
+	}
+
+	// return the capacity of the string()
+	int capacity() const {
+		return m_size;
+	}
+
+	// clear() function
+	void clear() {
+		// set all of it to 0s (m_data[0] = 0 would have worked as well but this is thorough clear)
+		memset(m_data, 0, strlen(m_data)); 
+		m_size = 0;
+	}
+
+	// insert() function (only implementing inserting a full string before position pos)
+	string& insert(int pos, const string& str2) {
+		if (pos >= m_size || pos < 0) {
+			return *this;
+		}
+
+		char* m_new_data = new char[m_size + str2.m_size + 1];
+
+		// if inserting at 0th position
+		if (pos == 0) {
+			std::strncpy(m_new_data, str2.m_data, str2.m_size);
+			std::strcat(m_new_data, m_data);
+		}
+		
+		else {
+			std::strncpy(m_new_data, m_data, pos);
+			std::strcat(m_new_data, str2.m_data);
+			std::strcat(m_new_data, m_data + pos);
+		}
+
+		delete [] m_data;
+		m_size = m_size + str2.m_size + 1;
+		m_data = m_new_data;
+
+		return *this;
+	}
+
+	// erase() function : non iterator version and no default length
+	string& erase (size_t pos, size_t len) {
+
+		if (pos >= m_size || pos < 0 || (len + pos) > m_size) {
+			return *this;
+		}
+
+		char* m_new_data = new char[m_size];
+		
+		// if deleting from 0th position
+		if (pos == 0) {
+			std::strcpy(m_new_data, m_data + len);
+		}
+
+		else {
+			std::strncpy(m_new_data, m_data, pos);
+			std::strcat(m_new_data, m_data + pos + len);
+		}
+
+		delete [] m_data;
+		m_data = m_new_data;
+
+		return *this;
+	}
+
+	// compare() function : only full string, no substring
+	int compare (const string& str) const {
+		return std::strcmp(m_data, str.m_data);
+	}
+
+	// append() function : basic one
+	string& append (const string& str) {
+		*this += str;
+		return *this;
+	}
+
 	~string(){
 		delete [] m_data;
 		m_data = nullptr;
@@ -58,6 +144,15 @@ class string{
 	}
 
 	inline string& operator+=(string& left, const string& right){
+
+		// Need to put this condition to increase size if needed to accomodate new string
+		if (int s = (left.size() + right.size() + 1); s > left.m_size) {
+			char* m_new_data = new char[s];
+			std::strcpy(m_new_data, left.m_data);
+			delete [] left.m_data;
+			left.m_data  = m_new_data;
+			left.m_size = s;
+		}
 		std::strcat(left.m_data,right.m_data);
 		return left;
 	}
